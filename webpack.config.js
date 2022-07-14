2adc192c40932f671ab5b67e5c778721108e1e5a
korkt-kim <gnscott6@naver.com>
2022-07-14 23:40:26 +0900

    fix: mode production, UI Language 한글로 변경
    - 메모리 줄이기 일환

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' 
 const TerserWebpackPlugin = require( 'terser-webpack-plugin' );
 
 module.exports = {
+	mode:'production',
 	devtool: 'source-map',
 	performance: { hints: false },
 
@@ -48,7 +49,7 @@ module.exports = {
 		new CKEditorWebpackPlugin( {
 			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
 			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
-			language: 'ko',
+			language: 'en',
 			additionalLanguages: 'all'
 		} ),
 		new webpack.BannerPlugin( {
